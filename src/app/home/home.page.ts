e33f4e7c618eaf1718b0fd6a38646b20938920c7
davinen_s <davinen.s@gmail.com>
2019-05-28 00:25:20 +0400

    Add Todo button implemented.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  todos = [];
+  constructor(private alertController: AlertController) {}
 
+
+  /**
+   * Opens a popup for user to add a Todo Task.
+   */
+  async addTodoAlert() {
+    const alert = await this.alertController.create({
+      header: 'Add a Todo',
+      message: 'Enter Your Todo',
+      inputs: [
+        {
+          type: 'text',
+          name: 'addTodoInput'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel', // add dismiss function.
+          cssClass: 'secondary', // add theme secondary styling.
+          handler: () => {
+            console.log('cancel button pressed..');
+          }
+        },
+        {
+          text: 'Add Todo',
+          handler: (inputData) => { // inputData contains all data from all your inputs from the alert.
+            const todoText = inputData.addTodoInput; // access the input by its name defined.
+            this.todos.push(todoText);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    }
 }
